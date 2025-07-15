f14ec88639dbbe54096470b94ad73bba73ff4a01
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-07-15 19:23:26 +0200

    feat: Add user app

--- airport_rest_api/settings.py
+++ airport_rest_api/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "airport",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -98,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
