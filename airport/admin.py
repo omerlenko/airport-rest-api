4adbdae15067f9114ca70de2589e4c78f3a57581
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-07-15 20:18:45 +0200

    feat: Add models to admin panel

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import (
+    Country,
+    City,
+    Airport,
+    Route,
+    CrewMember,
+    AirplaneType,
+    Airplane,
+    SeatClass,
+    Ticket,
+    Order,
+    Flight
+)
+
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Airport)
+admin.site.register(Route)
+admin.site.register(CrewMember)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(SeatClass)
+admin.site.register(Flight)
+admin.site.register(Ticket)
+admin.site.register(Order)
