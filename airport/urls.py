5cf5cca2c790228027adcd4051e75e7155e4bcc0
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-09-11 14:31:34 +0200

    feat: Register routers for crew_members, airplane_types, airplanes, flights, seat_classes, tickets and orders

--- airport/urls.py
+++ airport/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from airport.views import CountryViewSet, CityViewSet, AirportViewSet, RouteViewSet
+from airport.views import CountryViewSet, CityViewSet, AirportViewSet, RouteViewSet, CrewMemberViewSet, \
+    AirplaneTypeViewSet, AirplaneViewSet, FlightViewSet, SeatClassViewSet, OrderViewSet, TicketViewSet
 
 router = routers.DefaultRouter()
 router.register("countries", CountryViewSet)
 router.register("cities", CityViewSet)
 router.register("airports", AirportViewSet)
 router.register("routes", RouteViewSet)
+router.register("crew_members", CrewMemberViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("flights", FlightViewSet)
+router.register("seat_classes", SeatClassViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
