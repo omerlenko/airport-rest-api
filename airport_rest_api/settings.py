09f02d9abb8a5bf4ba8326bf2de1848cd7dbd51e
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-09-11 13:46:49 +0200

    feat: Add global datetime format in settings

--- airport_rest_api/settings.py
+++ airport_rest_api/settings.py
@@ -131,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DATETIME_FORMAT": "%Y-%m-%d %H:%M"
+}
