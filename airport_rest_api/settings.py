db5a3a0854474d16d1e595483f87590f4264b144
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-10-17 18:56:06 +0200

    feat: Set up throttling

--- airport_rest_api/settings.py
+++ airport_rest_api/settings.py
@@ -140,6 +140,14 @@ REST_FRAMEWORK = {
     ),
     "DATETIME_FORMAT": "%Y-%m-%d %H:%M",
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+       "rest_framework.throttling.AnonRateThrottle",
+       "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+       "anon": "100/day",
+       "user": "1000/day"
+    }
 }
 
 SIMPLE_JWT = {
